820b4fab232795c8261c23d22dad4cbdec2bbcdb
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-09-04 09:40:47 +0530

    [Vaibhav] added refactored code to lambda expression to validate user entry

--- src/main/java/com/bridgelabz/userregistrationjava/Regex.java
+++ src/main/java/com/bridgelabz/userregistrationjava/Regex.java
@@ -3,38 +3,54 @@ package com.bridgelabz.userregistrationjava;
 import java.util.regex.Pattern;
 import java.util.regex.Pattern;
 
-public class Regex 
+@FunctionalInterface
+interface Validation
 {
-	public boolean validFirstName(String firstName) 
+	boolean validateUserEntry(String entry);
+}
+public class Regex
+{
+	public static void main(String args[])
 	{
+	Validation validFirstName=(String firstName)->{
 		String reg="^[A-Z][a-z]{2,}";
 		Pattern p=Pattern.compile(reg);
 		return p.matcher(firstName).matches();
-	}
-
-	public boolean validLastName(String lastName) 
-	{
+	};
+	boolean result = validFirstName.validateUserEntry("Vaibhav");
+	System.out.println("First name is valid = "+result);
+	
+	Validation validLastName=(lastName)->{
 		String reg="^[A-Z][a-z]{2,}";
 		Pattern p=Pattern.compile(reg);
 		return p.matcher(lastName).matches();
-	}
-	public boolean validMobileNo(String number)
-	{
+	};
+	result = validLastName.validateUserEntry("Kokate");
+	System.out.println("Last name is valid = "+result);
+	
+	Validation validMobileNo=(String mobileNo)->{
 		String reg="[0-9]{2}\\s[0-9]{10}";
 		Pattern p=Pattern.compile(reg);
-		return p.matcher(number).matches();
-	}
-	public static boolean validEmail(String email)
-	{
-		String reg=("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$");
-		Pattern p=Pattern.compile(reg, Pattern.CASE_INSENSITIVE);
+		return p.matcher(mobileNo).matches();
+	};
+	result = validMobileNo.validateUserEntry("91 9987345234");
+	System.out.println("Mobile number is valid = "+result);
+	
+	Validation validEmail=(String email)->{
+		String reg="^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+		Pattern p=Pattern.compile(reg);
 		return p.matcher(email).matches();
-	}
-	public static boolean validPassword(String password)
-	{
+	};
+	result = validEmail.validateUserEntry("Vaibhavkokate123@gmail.com");
+	System.out.println("Email is valid = "+result);
+	
+	Validation validPassword=(String password)->{
 		String reg="[a-z[0-9]+[A-Z]+[@#$]{1}+]{8,}";
 		Pattern p=Pattern.compile(reg);
 		return p.matcher(password).matches();
-	}
+	};
+	result = validPassword.validateUserEntry("Ajay@345");
+	System.out.println("Password is valid = "+result);
 
-}
+
+}}
